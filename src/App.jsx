9b98223915f71fd481a156c8fd04115c8745e7ef
sasuke914 <powjsnxngapaons@gmail.com>
2024-11-11 09:11:49 -0800

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useEffect } from 'react';
-import Layout from './components/Layout/Layout';
-import Home from './pages/Home';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function App() {
   useEffect(() => {
@@ -19,5 +19,6 @@ function App() {
     </BrowserRouter>
   );
 }
+// git
 
 export default App;
